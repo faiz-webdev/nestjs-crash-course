73fee455266eeb54e6adbcad449662cf70c54186
faiz-webdev <faijuddin@tothenew.com>
2023-07-18 11:38:26 +0530

    7. applying middleware at route and global level

--- src/user-module/user.module.ts
+++ src/user-module/user.module.ts
@@ -1,6 +1,12 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { LoggerMiddleware } from './middleware';
 // import { AppController } from './user.controller';
 // import { AppService } from './user.service';
 
@@ -10,4 +16,12 @@ import { UserService } from './user.service';
   providers: [UserService],
   exports: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: 'users', method: RequestMethod.GET })
+      .forRoutes(UserController);
+    //   .forRoutes({ path: 'users', method: RequestMethod.POST });
+  }
+}
